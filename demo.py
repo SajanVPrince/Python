4caa43356271c0465d9ef5beda668fcd7b36cef0
SajanVPrince <sajanvprince@gmail.com>
2024-08-05 15:58:40 +0530

    datatype added

---
+++ demo.py
@@ -0,0 +1,35 @@
+# print("Helooo")
+# print("Hei")
+# print("Sajan Here...")
+
+name = "Sajan v prince"
+print("name=",name)
+print(type(name))
+number = 9207123298
+print("number=",number)
+print(type(number))
+weight =70.7
+print("weight=",weight)
+print(type(weight))
+a=19+5j
+print("complex",a)
+print(type(a))
+aa=True
+print("boolean",aa)
+print(type(aa))
+bb=False
+print("boolean",bb)
+print(type(bb))
+names=["Roshan","jith","Aromal"]
+print("list",names)
+print(type(names))
+ages=(15,34,12)
+print("ages",ages)
+print(type(ages))
+set={11,12,13}
+print("sets",set)
+print(type(set))
+data={'date':12202}
+print("date",data)
+print(type(data))
+
