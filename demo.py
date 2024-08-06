724b7d4526e39cb7551ed11b54ac8b6f87305924
SajanVPrince <sajanvprince@gmail.com>
2024-08-06 17:51:20 +0530

    product page added

--- demo.py
+++ demo.py
@@ -2,34 +2,64 @@
 # print("Hei")
 # print("Sajan Here...")
 
-name = "Sajan v prince"
-print("name=",name)
-print(type(name))
-number = 9207123298
-print("number=",number)
-print(type(number))
-weight =70.7
-print("weight=",weight)
-print(type(weight))
-a=19+5j
-print("complex",a)
-print(type(a))
-aa=True
-print("boolean",aa)
-print(type(aa))
-bb=False
-print("boolean",bb)
-print(type(bb))
-names=["Roshan","jith","Aromal"]
-print("list",names)
-print(type(names))
-ages=(15,34,12)
-print("ages",ages)
-print(type(ages))
-set={11,12,13}
-print("sets",set)
-print(type(set))
-data={'date':12202}
-print("date",data)
-print(type(data))
+# data Types
 
+# name = "Sajan v prince"
+# print("name=",name)
+# print(type(name))
+# number = 9207123298
+# print("number=",number)
+# print(type(number))
+# weight =70.7
+# print("weight=",weight)
+# print(type(weight))
+# a=19+5j
+# print("complex",a)
+# print(type(a))
+# aa=True
+# print("boolean",aa)
+# print(type(aa))
+# bb=False
+# print("boolean",bb)
+# print(type(bb))
+# names=["Roshan","jith","Aromal"]
+# print("list",names)
+# print(type(names))
+# ages=(15,34,12)
+# print("ages",ages)
+# print(type(ages))
+# set={11,12,13}
+# print("sets",set)
+# print(type(set))
+# data={'date':12202}
+# print("date",data)
+# print(type(data))
+
+# Revive a data
+
+
+# Name = input("Enter your name : ")
+# Age = int(input("Enter your age : "))
+# Weight = float(input("Enter your weight : "))
+
+# print("name :",Name)
+# print("age :",Age)
+# print("Weight :",Weight)
+
+# typecast
+
+'''a="sajan"
+print(type(a))
+b=str(a)
+print(type(b))'''
+
+# print("hello \t all")
+# print("hello \nall \nhere")
+
+# print("""hello
+# i am
+# here""")
+
+item=input("enter an item : ")
+price=int(input("enter the price : "))
+print(f"its a {item}, and its price is {price} rs")
