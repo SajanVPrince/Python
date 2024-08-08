16c9f8e2a2d9dd505bef37888f929980b6eff5cf
SajanVPrince <sajanvprince@gmail.com>
2024-08-08 15:57:25 +0530

    If Statement

---
+++ conditional/if.py
@@ -0,0 +1,17 @@
+# check whether the number is same or not 
+
+'''a=int(input("enter a number : "))
+b=int(input("enter a number : "))
+if a==b:
+    print("True")
+else:
+    print("Flase")'''
+
+# Check which is the largest number
+
+a=int(input("enter a number : "))
+b=int(input("enter a number : "))
+if a>b:
+    print("Largest Number :",a)
+else:
+    print("Largest Number :",b)
---
+++ string.py
@@ -0,0 +1,24 @@
+'''a="sajanvprince"
+print(a)
+print(a[5])
+print(a[2:5])'''
+
+# a='sajanvprince'
+# print(a[::-1])
+
+a='           sajanvprince            '
+# print(a.capitalize())
+# print(a.count('a'))
+# print(a.center(50))
+# print(a.endswith('n'))
+# print(a.find('e'))
+# print(a.index('s'))
+# print(a.isdigit())
+# print(a.islower())
+# print(a.strip())
+
+# print("{:<10}{:<8}{:<8}".format("Name","age","mark"))
+# print("{:<10}{:<8}{:<8}".format("A","20","54"))
+# print("{:<10}{:<8}{:<8}".format("A","20","54"))
+
+
