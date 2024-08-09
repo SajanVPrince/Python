ec1d2c3e078beec89b86504c23076382d2d73446
SajanVPrince <sajanvprince@gmail.com>
2024-08-09 15:57:22 +0530

    nested if

--- conditional/if.py
+++ conditional/if.py
@@ -9,9 +9,25 @@ else:
 
 # Check which is the largest number
 
-a=int(input("enter a number : "))
+'''a=int(input("enter a number : "))
 b=int(input("enter a number : "))
 if a>b:
     print("Largest Number :",a)
 else:
-    print("Largest Number :",b)
+    print("Largest Number :",b)'''
+
+# largest of three numbers using nested if
+
+'''a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if a>b:
+    if a>c:
+        print("Largest is ",a)
+    else:
+        print("largest is ",c)
+else:
+    if b>c:
+        print("largest is ",b)
+    else:
+        print("largest is ",c)'''
