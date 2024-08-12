e3ea55fa9386bc7488e8c0e7259d8eb427eb0314
SajanVPrince <sajanvprince@gmail.com>
2024-08-12 09:16:42 +0530

    tasks

--- conditional/if.py
+++ conditional/if.py
@@ -31,3 +31,59 @@ else:
         print("largest is ",b)
     else:
         print("largest is ",c)'''
+
+# Task 1
+
+'''s=int(input("Enter Your Salary :"))
+e=int(input("How many years of service do you have? :"))
+
+if e>5:
+    a=s*5/100
+    ts=a+s
+    print("Total Salary is :",ts)
+    print("Bonus",a)
+else:
+    print("Total Salary is :",s)
+    print("Bonus = 0")'''
+
+# Task 2
+
+'''u=int(input("Enter your electricity unit : "))
+
+if u<100:
+    print("Your unit price = 0")
+else:
+    if u<200 and u>100:
+        up=u-100
+        ua=up*5
+        print("your unit price is",ua)
+    else:
+        ui=u-200
+        us=ui*10+500
+        # 500 default value of 100 unit when it is 5/unit
+        print("Your unit price is",us)'''
+
+# Task 3
+
+'''a=int(input("Enter a number between 1 to 7 : "))
+
+if a==1:
+    print("Sunday")
+elif a==2:
+    print("Monday")
+elif a==3:
+    print("Tuesday")
+elif a==4:
+    print("Wednesday")
+elif a==5:
+    print("Thursday")
+elif a==6:
+    print("Friday")
+elif a==7:
+    print("Saturday")
+else:
+    print("Invalid Number")'''
+
+
+# task 4
+
