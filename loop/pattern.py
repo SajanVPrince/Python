8be5a81efa27693a9d506a4bc11fe2b6c32e0ffb
SajanVPrince <sajanvprince@gmail.com>
2024-08-20 09:04:25 +0530

    Pattern

---
+++ loop/pattern.py
@@ -0,0 +1,48 @@
+# pattern
+
+'''for i in range(3):
+    for j in range(3):
+        print(j,end=' ')
+    print()'''
+
+# Task 1
+# 0 1 2
+# 3 4 5 
+# 6 7 8
+
+'''a=0
+for i in range(3):
+    for j in range(3):
+        print(a,end=' ')
+        a+=1
+    print()'''
+
+# Task 2
+# 0 1 2
+# 1 2 3
+# 2 3 4
+
+
+'''for i in range(3):
+    for j in range(3):
+        print(i+j,end=' ')
+    print()'''
+
+# task 3
+# 0 1 2
+# 2 1 0
+# 0 1 2
+# 2 1 0
+
+'''a=0
+for i in range(4):
+    for j in range(3):
+        if i % 2 == 0:
+            print(j, end=" ")
+        else:
+            print(2-j,end=' ')
+    print()'''
+    
+
+      
+
