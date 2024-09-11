bccb158639038d86da7744dae448ddba76657cb8
SajanVPrince <sajanvprince@gmail.com>
2024-09-11 09:06:02 +0530

    arguments

---
+++ func/Arguments.py
@@ -0,0 +1,20 @@
+# Position Arguments
+# to collect arguments is called parameters
+# no. of parameters = no. of arguments
+
+# eg1:
+'''def sample(a,b):
+    print(a,b)
+sample('a','b')
+sample(10,20)
+sample('a',20)
+sample([1,2,3],'g')'''
+
+# Function with a keyword
+
+'''def sample(name,age):
+    print('name : ',name)
+    print('age :',age)
+sample('anu',20)
+sample(20,'arun')
+sample(age=20,name='akhil')'''
--- func/functions.py
+++ func/functions.py
@@ -24,3 +24,54 @@ print(a)
 sample()
 # print(b)'''
 
+# Global keyword
+
+'''def sample():
+    global a
+    a=10
+    print(a)
+sample()
+print(a)'''
+
+# Return Keyword
+
+'''def sample():
+    a=10
+    b=19
+    return 'WELCOME',a,b
+a,b,c=sample()
+print(a)
+print(b)
+print(c)'''
+
+# Task 1
+
+'''def num():
+    a=int(input('enter a number : '))
+    b=int(input('enter a number : '))
+    return a,b
+while True:
+    print(''''''
+    1. add
+    2. sub
+    3. mul
+    4. divi
+    5. exit'''''')
+
+    c=int(input('enter the number : '))
+    if c==1:
+        a,b=num()
+        print(a+b)
+    elif c==2:
+        a,b=num()
+        print(a-b)
+    elif c==3:
+        a,b=num()
+        print(a*b)
+    elif c==4:
+        a,b=num()
+        print(a//b)
+    elif c==5:
+        break
+    else:
+        print('Choose Correct number')'''
