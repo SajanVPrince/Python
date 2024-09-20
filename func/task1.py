f6625f388d1e08b83241108b03b4f358e1c9ca9e
SajanVPrince <sajanvprince@gmail.com>
2024-09-20 21:12:30 +0530

    Admin work done

--- func/task1.py
+++ func/task1.py
@@ -33,7 +33,7 @@ def login():
             u=i
     return f,u
 
-def add():
+def add_bk():
     print('ADD BOOK')
     if len(lib)==0:
         id=1
@@ -44,10 +44,49 @@ def add():
     stock=int(input('enter the stock availible : '))
     lib.append({'id':id,'name':name,'price':price,'stock':stock})
 
+def view_bk():
+    print('BOOK DETAILS')
+    print("{:<5}{:<10}{:<10}{:<10}".format('ID','BOOKNAME','PRICE','STOCK'))
+    print('_'*30)
+    for i in lib:
+        print("{:<5}{:<10}{:<10}{:<10}".format(i['id'],i['name'],i['price'],i['stock']))
+
+def update_bk():
+    id=int(input('enter the id : '))
+    f=0
+    for i in lib:
+        if i['id']==id:
+            price=int(input('enter the price : '))
+            stock=int(input('enter the stock : '))
+            i['price']=price
+            i['stock']=stock
+            print('Details Updated')
+            f=1
+    if f==0:
+        print('invalid id')
+
+def delete_bk():
+    id=int(input('enter the id : '))
+    f=0
+    for i in lib:
+        if i['id']==id:
+            lib.remove(i)
+            print('data deleted')
+            f=1
+    if f==0:
+        print('Invalid id')
+
+def view_usr():
+    print('USERS DETAILS')
+    print("{:<10}{:<15}{:<15}{:<15}".format('ID','NAME','EMAIL','PHONE'))
+    print('_'*55)
+    for i in user:
+        print("{:<10}{:<15}{:<15}{:<15}".format(i['id'],i['name'],i['email'],i['phone']))
+
 # Fuctions end
 
 user=[{'id': 1000, 'name': 'sajan', 'email': 's@','phone': 920712, 'password': 'asdf'}]
-lib=[]
+lib=[{'id': 1, 'name': 'aa', 'price': 20, 'stock': 2},{'id': 2, 'name': 'ab', 'price': 200, 'stock': 2}]
 while True:
     print('''
     1.Register as User
@@ -70,7 +109,19 @@ while True:
 
                 c1=int(input('enter your choice : '))
                 if c1==1:
-                    add()
+                    add_bk()
+                elif c1==2:
+                    view_bk()
+                elif c1==3:
+                    update_bk()
+                elif c1==4:
+                    delete_bk()
+                elif c1==5:
+                    view_usr()
+                elif c1==6:
+                    break
+                else:
+                    print('invalid Choice')
         elif f==2:
             print('user login')
         else:
