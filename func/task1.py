93dd01387efcfa8bfc80f2a95563dcdc3dcdf09d
SajanVPrince <sajanvprince@gmail.com>
2024-09-20 09:32:46 +0530

    lib management

--- func/task1.py
+++ func/task1.py
@@ -1,20 +1,52 @@
+# Functions
+
 def register():
     print('Registration Page')
-    email=input('enter your email :')
+    if len(user)==0:
+        id=1000
+    else:
+        id=user[-1]['id']+1
+    email=str(input('enter your email :'))
     f=0
     for i in user:
         if i['email']==email:
             f=1
+            print('email already exists enter another one')
             register()
     if f==0:
         name=str(input('enter your name : '))
-        id=int(input('enter an id : ')) 
-        username=email
         phone=int(input('enter your number : '))
         password=input('enter the password : ')
-        user.append({'id':id,'name':name,'email':email,'username':username,'phone':phone,'password':password})
+        print('Registration Succesfull email id is your username')
+        user.append({'id':id,'name':name,'email':email,'phone':phone,'password':password})
 
-user=[]
+def login():
+    usern=input('Enter Username : ')
+    passw=input('Enter password : ')
+    f=0
+    u=''
+    if usern=='admin' and passw=='admin':
+        f=1
+    for i in user:
+        if usern==i['email'] and passw==i['password']:
+            f=2
+            u=i
+    return f,u
+
+def add():
+    print('ADD BOOK')
+    if len(lib)==0:
+        id=1
+    else:
+        id=lib[-1]['id']+1
+    name=str(input('enter name : '))
+    price=int(input('enter the price : '))
+    stock=int(input('enter the stock availible : '))
+    lib.append({'id':id,'name':name,'price':price,'stock':stock})
+
+# Fuctions end
+
+user=[{'id': 1000, 'name': 'sajan', 'email': 's@','phone': 920712, 'password': 'asdf'}]
 lib=[]
 while True:
     print('''
@@ -24,5 +56,26 @@ while True:
     c=int(input('enter your choice : '))
     if c==1:
         register()
-    for i in user:
-        print(i)
+    elif c==2:
+        f,u=login()
+        if f==1:
+            while True:
+                print('''
+                1.Add book
+                2.View Book
+                3.Update Book Details
+                4.Delete book
+                5.View Users
+                6.Logout''')
+
+                c1=int(input('enter your choice : '))
+                if c1==1:
+                    add()
+        elif f==2:
+            print('user login')
+        else:
+            print('invalid username or password')
+    elif c==3:
+        break
+    else:
+        print('Invalid Choice')
