484ef14a3c8a37162418078ce80eef914f9d96ed
SajanVPrince <sajanvprince@gmail.com>
2024-09-26 09:13:08 +0530

    write mode

--- file_handling/file_handling.py
+++ file_handling/file_handling.py
@@ -35,7 +35,7 @@
 
 # File to be readed by using read mode
 
-f=open('demo.txt','r')
+# f=open('demo.txt','r')
 
 # READ
 # ----
@@ -77,12 +77,77 @@ f=open('demo.txt','r')
 
 # print lines using for loop
 
-a=f.readlines()
-l=len(a)
-f.seek(0)
-for i in range(l):
-    b=f.readline() .strip()
-    c=len(b)
-    print(c)
-    # print(b)
-    print(b[::-1])  #to reverse a number
+# to find letters in a file and reverse the letters
+# -------------------------------------------------
+
+# a=f.readlines()
+# l=len(a)
+# f.seek(0)
+# letter=0
+# capital=0
+# for i in range(l):
+#     b=f.readline() .strip()
+#     # print(b[::-1])  #to reverse a number
+#     for i in b:
+#         if i!=' ':
+#             letter+=1
+#     print(letter)
+
+
+# to find capital and small letters
+# ---------------------------------
+
+# a=f.readlines()
+# l=len(a)
+# f.seek(0)
+# letter=0
+# cap=0
+# for i in range(l):
+#     b=f.readline() .strip()
+#     for i in b:
+#         print(i)
+#         if i!=' ':
+#             letter+=1
+#             if i.isupper():
+#                 cap+=1
+            
+# print('capital',cap)
+# print('small',letter-cap)
+
+# to find words in a txt file
+# ---------------------------
+
+# a=f.readlines()
+# l=len(a)
+# f.seek(0)
+# w=0
+# for i in range(l):
+#     b=f.readline() .strip()
+#     s=b.split(' ')
+#     for j in s:
+#         if j!='':
+#             w+=1
+# print(w)
+
+# WRITE
+# -----
+
+# if already file exists it will over write the content
+
+f=open('demo.txt','w')
+# f.write('welcome\n')
+# f.write('123\n')
+# f.write('hi '+'hello\n')
+
+# if file doesn't exist it will create a new file and write the content
+
+# f=open('test.txt','w')
+# f.write('welcome\n')
+# f.write('123\n')
+# f.write('hi '+'hello\n')
+
+a=int(input('enter a number : '))
+for i in range(1,11):
+    x=a*i
+    x=str(x)
+    f.write(f"{a} x {i} = {x}\n")
