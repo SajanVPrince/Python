cfb8a6733a87bfb1af608444a6d3b36d71fb4cdd
SajanVPrince <sajanvprince@gmail.com>
2024-10-04 09:28:21 +0530

    Operators

---
+++ operators/operators.py
@@ -0,0 +1,49 @@
+# Arithmetic operators
+
+'''print("Addition : ",10+5)
+print("Subtraction : ",10-5)
+print("Multiplication : ",10*5)
+print("Division : ",10/5)
+print("Modules : ",10%5)
+print("floor : ",10//3,10/3)
+print("power: ",10**3)'''
+
+# Assignment operators
+
+'''a=10
+print(a)
+a=10
+a+=5
+print(a)
+a=10
+a-=5
+print(a)
+a=10
+a/=5
+print(a)
+a=10
+a*=5
+print(a)
+a=10
+a%=3
+print(a)
+a=10
+a//=3
+print(a)
+a=10
+a**=3
+print(a)'''
+
+# comparison Operators
+
+# print(10==9)
+
+# logical operators
+
+# print('e' in 'welcome')
+
+# a=10
+# b=10
+# print(a is b)
+# print(id(a))
+# print(id(b))
